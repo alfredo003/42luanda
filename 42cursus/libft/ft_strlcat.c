02463742e4edfb5203246f30e78ca93b05ceb6e3
achivela <achivela@student.42luanda.com>
2024-05-14 18:33:36 +0100

    updated

--- 42cursus/libft/ft_strlcat.c
+++ 42cursus/libft/ft_strlcat.c
@@ -9,37 +9,34 @@
 /*   Updated: 2024/05/14 15:55:46 by achivela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-static size_t	str_len(char const *str)
-{
-	size_t	i;
 
-	i = 0;
-	while (*(str + i))
-		i++;
-	return (i);
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
 }
 
 size_t	ft_strlcat(char *dest, const char *src, size_t n)
 {
-	size_t		dest_len;
-	size_t		total_len;
-	const char	*s;
+	size_t	dest_len;
+	size_t	i;
+	size_t	total_len;
 
-	if ((!dest || !src) && !n)
-		return (0);
-	s = src;
-	dest_len = 0;
-	while (*(dest + dest_len) && dest_len < n)
-		dest_len++;
-	if (dest_len < n)
-		total_len = dest_len + str_len(s);
-	else
-		return (n + str_len(s));
-	while (*s && (dest_len + 1) < n)
+	dest_len = ft_strlen(dest);
+	total_len = dest_len + ft_strlen(src);
+	if (n <= dest_len)
+		return (total_len - (dest_len - n));
+	i = 0;
+	while (src[i] && dest_len + 1 < n)
 	{
-		*(dest + dest_len) = *s++;
+		dest[dest_len] = src[i];
 		dest_len++;
+		i++;
 	}
-	*(dest + dest_len) = '\0';
+	dest[dest_len] = '\0';
 	return (total_len);
 }

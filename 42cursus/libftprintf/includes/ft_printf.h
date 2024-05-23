48dd5eaab9d930ddde13b012e8faf5b2fb019d30
achivela <achivela@student.42luanda.com>
2024-05-23 15:15:16 +0100

    updated

---
+++ 42cursus/libftprintf/includes/ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achivela <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/23 15:08:08 by achivela          #+#    #+#             */
+/*   Updated: 2024/05/23 15:08:11 by achivela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+void			ft_putcharacter_length(char character, int *length);
+void			ft_string(char *args, int *length);
+void			ft_number(int number, int *length);
+void			ft_hexadecimal(unsigned int x, int *length, char x_or_x);
+void			ft_unsigned_int(unsigned int u, int *length);
+void			ft_pointer(size_t pointer, int *length);
+
+int				ft_printf(const char *string, ...);
+
+#endif
---
+++ 42cursus/libftprintf/sources/ft_words.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_words.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achivela <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/23 15:09:53 by achivela          #+#    #+#             */
+/*   Updated: 2024/05/23 15:09:55 by achivela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+void	ft_putcharacter_length(char character, int *length)
+{
+	write(1, &character, 1);
+	(*length)++;
+}
+
+void	ft_string(char *args, int *length)
+{
+	size_t	i;
+
+	i = 0;
+	if (!args)
+	{
+		write(1, "(null)", 6);
+		(*length) += 6;
+		return ;
+	}
+	while (args[i] != '\0')
+	{
+		ft_putcharacter_length(args[i], length);
+		i++;
+	}
+}
